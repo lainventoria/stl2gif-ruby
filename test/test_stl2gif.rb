9efefd307c3bfa6a57bdca06c6472bbbb280e550
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-12-16 15:22:50 -0300

    Extract related tests using spec style

--- test/test_stl2gif.rb
+++ test/test_stl2gif.rb
@@ -5,26 +5,6 @@ class TestStl2gif < MiniTest::Spec
     refute_nil ::Stl2gif::VERSION
   end
 
-  def test_point_returns_pov
-    Stl2gif::Point.new([0,1,2]).to_pov.must_equal '<0,1,2>'
-  end
-
-  def test_triagle_returns_pov
-    Stl2gif::Triangle.new([[1,0,0],[0,1,0],[0,0,1]]).to_pov.must_equal 'triangle { <1,0,0>, <0,1,0>, <0,0,1> }'
-  end
-
-  def test_mesh
-    Stl2gif::Mesh.new([[[1,0,0],[0,1,0],[0,0,1]],[[-1,0,0],[0,-1,0],[0,0,-1]]])
-  end
-
-  def pov_test_mesh
-    'mesh { triangle { <1,0,0>, <0,1,0>, <0,0,1> }, triangle { <-1,0,0>, <0,-1,0>, <0,0,-1> } }'
-  end
-
-  def test_triangles_becomes_pov
-    test_mesh.to_pov.must_equal pov_test_mesh
-  end
-
   def test_stl
     Stl2gif::Stl.new(File.expand_path('../fixtures/test.stl',__FILE__))
   end
