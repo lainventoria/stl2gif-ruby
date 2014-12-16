35081bc31e69e5b08dec9bf23d04ef6525c59598
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-12-16 15:43:54 -0300

    Use spec style for frame generation tests

--- test/test_stl2gif.rb
+++ test/test_stl2gif.rb
@@ -1,30 +1,37 @@
 require 'minitest_helper'
 
-class TestStl2gif < MiniTest::Spec
-  def test_that_it_has_a_version_number
-    refute_nil ::Stl2gif::VERSION
+describe Stl2gif do
+  it 'has a version number' do
+    Stl2gif::VERSION.wont_be_nil
   end
 
-  def test_stl
-    Stl2gif::Stl.new(File.expand_path('../fixtures/test.stl',__FILE__))
-  end
+  describe Stl2gif::Stl do
+    subject { Stl2gif::Stl.new(File.expand_path('../fixtures/test.stl',__FILE__)) }
 
-  def test_frame_generation
-    s = test_stl 
-    s.render_frame 0
-    s.render_frame 0.35 * Math::PI
-    s.frames.size.must_equal 2
-    s.clear_temp_files
-    s.frames.size.must_equal 0
-  end
+    before do
+      subject.render_frame 0
+      subject.render_frame 0.35 * Math::PI
+      subject.render_frame 0.5 * Math::PI
+    end
 
-  def test_frames_become_gif
-    s = test_stl
-    s.render_frame 0
-    s.render_frame 0.25 * Math::PI
-    s.render_frame 0.5 * Math::PI
-    s.to_gif('test.gif')
-    s.clear_temp_files
-    File.exists?('test.gif').must_be true
+    after do
+      subject.clear_temp_files
+    end
+
+    it 'generates frames' do
+      subject.frames.size.must_equal 3
+    end
+
+    it 'clears frames' do
+      subject.clear_temp_files
+
+      subject.frames.size.must_equal 0
+    end
+
+    it 'converts to gif' do
+      subject.to_gif 'test.gif'
+
+      File.exists?('test.gif').must_be true
+    end
   end
 end
